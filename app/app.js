05e3fb41fc4f43836ccbbcf43f0cf893303545bf
Jared <jst9259@gmail.com>
2017-08-01 18:25:44 -0400

    log in react

--- app/app.js
+++ app/app.js
@@ -22,18 +22,20 @@ var Main = require("./components/Main");
 var Form = require("./components/children/Form");
 var JobForms = require("./components/children/JobForms");
 var Table = require("./components/children/Table");
+var Login = require("./components/children/Login");
+// var Signup = require("./components/children/Signup");
 
 
 // This code here allows us to render our main component (in this case Main)
 ReactDOM.render(<Router history={browserHistory}>
-    <Route path="/" component={Main}>
+    <Route path="/" component={Login}>
 
       {/* If user selects Search or Saved show the appropriate component */}
       <Route path="Clients" component={Form} />
       <Route path="Jobs" component={JobForms} />
       <Route path="Table" component={Table} />
       {/* If user selects any other path... we get the Home Route */}
-      <IndexRoute component={Main} />
+      <IndexRoute component={Login} />
 
     </Route>
   </Router>, document.getElementById('app'));
